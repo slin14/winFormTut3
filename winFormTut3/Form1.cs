d1dd154e5d7c8b4e2fe88a631aa0277f12edc9fa
Sophie <sophielin14@gmail.com>
2023-09-01 16:16:52 -0700

    add all the labels and assign a random icon to each label

--- winFormTut3/Form1.cs
+++ winFormTut3/Form1.cs
@@ -12,12 +12,53 @@ namespace winFormTut3
 {
     public partial class Form1 : Form
     {
+
+        // Use this Random object to choose random icons for the squares
+        Random random = new Random();
+
+        // Each of these letters is an interesting icon
+        // in the Webdings font,
+        // and each icon appears twice in this list
+        List<string> icons = new List<string>()
+        {
+            "!", "!", "N", "N", ",", ",", "k", "k",
+            "b", "b", "v", "v", "w", "w", "z", "z"
+        };
+
+        /// <summary>
+        /// Assign each icon from the list of icons to a random square
+        /// </summary>
+        private void AssignIconsToSquares()
+        {
+            // The TableLayoutPanel has 16 labels,
+            // and the icon list has 16 icons,
+            // so an icon is pulled at random from the list
+            // and added to each label
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber]; // assigns one of the icons list items to the Text property of the label
+                    // iconLabel.ForeColor = iconLabel.BackColor; // hides the icons
+                    icons.RemoveAt(randomNumber); // removes the icon that has been added to the form from the list
+                }
+            }
+        }
         public Form1()
         {
             InitializeComponent();
+
+            AssignIconsToSquares();
         }
 
         private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
