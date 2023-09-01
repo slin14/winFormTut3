42c033f62970a55b4c22799a77324252914e5409
Sophie <sophielin14@gmail.com>
2023-09-01 16:43:17 -0700

    add CheckForWinner method and display congrats message when player wins

--- winFormTut3/Form1.cs
+++ winFormTut3/Form1.cs
@@ -109,6 +109,9 @@ namespace winFormTut3
                 secondClicked = clickedLabel;
                 secondClicked.ForeColor = Color.Black;
 
+                // Check to see if the player won
+                CheckForWinner();
+
                 // If the player clicked two matching icons, keep them 
                 // black and reset firstClicked and secondClicked 
                 // so the player can click another icon
@@ -149,6 +152,33 @@ namespace winFormTut3
             // clicked, the program knows it's the first click
             firstClicked = null;
             secondClicked = null;
+        }
+
+        /// <summary>
+        /// Check every icon to see if it is matched, by 
+        /// comparing its foreground color to its background color. 
+        /// If all of the icons are matched, the player wins
+        /// </summary>
+        private void CheckForWinner()
+        {
+            // Go through all of the labels in the TableLayoutPanel, 
+            // checking each one to see if its icon is matched
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+
+                if (iconLabel != null)
+                {
+                    if (iconLabel.ForeColor == iconLabel.BackColor)
+                        return;
+                }
+            }
+
+            // If the loop didn’t return, it didn't find
+            // any unmatched icons
+            // That means the user won. Show a message and close the form
+            MessageBox.Show("You matched all the icons!", "Congratulations");
+            Close();
         }
     }
 }
